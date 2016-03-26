b0d8d9543616277c165ddba9f281a7f124a749b7
nanalucky <nana_lucky@msn.com>
2016-03-26 22:58:48 +0800

    test NoTouchGUI

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;		// required for DllImport
+
 
 public class CameraController : MonoBehaviour {
 
 	public Canvas canvas;
 
-	// Use this for initialization
+	#if (UNITY_ANDROID && !UNITY_EDITOR)
+	[DllImport("OculusPlugin")]
+	// Support to fix 60/30/20 FPS frame rate for consistency or power savings
+	private static extern void OVR_TW_SetMinimumVsyncs( OVRTimeWarpUtils.VsyncMode mode );
+	#endif	// Use this for initialization
+
+
 	void Start () {
-	
+#if (UNITY_ANDROID && !UNITY_EDITOR)
+		// delay one frame because OVRCameraController initializes TimeWarp in Start()
+		Invoke("UpdateVSyncMode", 0.01666f);
+#endif
+#if (UNITY_ANDROID && !UNITY_EDITOR)
+		OVR_TW_SetMinimumVsyncs(OVRTimeWarpUtils.VsyncMode.VSYNC_60FPS);
+#endif	
 	}
 	
 	// Update is called once per frame
