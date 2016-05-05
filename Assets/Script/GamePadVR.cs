5fdea40a8740f6f24858cfc951d94870c8ecaf5b
nanalucky <nana_lucky@msn.com>
2016-05-05 17:09:29 +0800

    add gamepad

--- Assets/Script/GamePadVR.cs
+++ Assets/Script/GamePadVR.cs
@@ -7,7 +7,7 @@ public class GamePadVR : MonoBehaviour {
 	private float cameraHeightMin = 0.1f;
 	private float cameraHeightMax = 1.0f;
 	private float cameraHeightSpeed = 0.1f;
-	private float cameraRotateDistance = 1.5f;
+	private float cameraRotateSpeed = 60.0f;
 
 	private GameObject mainCamera;
 	private OVRCameraRig cameraController;
@@ -43,16 +43,24 @@ public class GamePadVR : MonoBehaviour {
 				if(Input.GetKeyDown(KeyCode.S))
 					primaryAxis.y = -1.0f;
 			}
-			if(primaryAxis.x != 0.0f || primaryAxis.y != 0.0f)
+			if(Mathf.Abs(primaryAxis.y) > Mathf.Abs(primaryAxis.x))
 			{
-				Vector3 fwd = cameraController.centerEyeAnchor.TransformDirection((new Vector3(primaryAxis.x, 0.0f, primaryAxis.y)).normalized);
-				fwd.y = 0.0f;
-				fwd.Normalize();
+				Vector3 euler = cameraController.transform.rotation.eulerAngles;
+				euler.x = euler.z = 0.0f;
+				Vector3 fwd = Matrix4x4.TRS( Vector3.zero, Quaternion.Euler(euler), new Vector3(1,1,1)).MultiplyVector((new Vector3(0.0f, 0.0f, primaryAxis.y)).normalized);
 
 				Vector3 pos = mainCamera.transform.position + fwd * (cameraSpeed * Time.deltaTime);
 				float x = Mathf.Clamp(pos.x, dogController.zoneMin.x, dogController.zoneMax.x);
 				float z = Mathf.Clamp(pos.z, dogController.zoneMin.z, dogController.zoneMax.z);
 				mainCamera.transform.position = new Vector3(x, mainCamera.transform.position.y, z);
+			}
+
+			// rotate
+			if(Mathf.Abs(primaryAxis.x) > Mathf.Abs(primaryAxis.y))
+			{
+				Vector3 euler = cameraController.transform.rotation.eulerAngles;
+				euler.y += primaryAxis.x * cameraRotateSpeed * Time.deltaTime;
+				cameraController.transform.rotation = Quaternion.Euler(euler);
 			}
 
 			// camera height
@@ -65,15 +73,6 @@ public class GamePadVR : MonoBehaviour {
 			y = Mathf.Clamp(y, cameraHeightMin, cameraHeightMax);
 			mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, y, mainCamera.transform.position.z);
 		
-			// rotate
-			float distance = (mainCamera.transform.position - goDog.transform.position).magnitude;
-			if(distance < cameraRotateDistance)
-			{
-				Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-				if(secondaryAxis.x != 0.0f)
-					RotateCamera (secondaryAxis.x);
-			}
-
 			// turn
 /*			if(Input.GetButtonDown (OVRGamepadController.ButtonNames [(int)OVRGamepadController.Button.LeftShoulder]))
 			{
@@ -82,21 +81,5 @@ public class GamePadVR : MonoBehaviour {
 				mainCamera.transform.rotation = Quaternion.Euler(euler);
 			}
 */		}
-	}
-
-	void RotateCamera(float mouseYOffset)
-	{
-		Vector3 lookat = goDog.transform.position;
-		lookat.y = mainCamera.transform.position.y;
-		float rayDist = (lookat - mainCamera.transform.position).magnitude;
-		
-		float mouseX = mainCamera.transform.rotation.eulerAngles.x;
-		float mouseY = mainCamera.transform.rotation.eulerAngles.y;	
-		
-		//mouseX -= Input.GetAxis("Mouse Y") * MouseSensitivity;
-		mouseY += mouseYOffset;
-		
-		mainCamera.transform.rotation = Quaternion.Euler(mouseX, mouseY, mainCamera.transform.rotation.eulerAngles.z);
-		mainCamera.transform.position = lookat + mainCamera.transform.rotation * (new Vector3(0, 0, -rayDist));
 	}
 }
